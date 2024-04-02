2a5fbebbf40ba5307cfb185ddf24fa122fb97047
dfolomeeva <d.folomeeva1987@gmail.com>
2024-04-02 17:07:51 +0300

    Tryed to outprint something.

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,20 @@ import java.util.ArrayList;
 
 public class Main {
     static ArrayList<Accomodation> accomodations = new ArrayList<>();
-    public static void main(String[] args) {
-        accomodations.add(new Flat(90,3,5000,
-                2));
-        accomodations.add(new HotelSuite(40,1,4500,
-                45));
-        accomodations.add(new House(200,5,1000,
-                2,8000));
 
-    System.out.print("Please choose the type of accomodation");
-    String str = new String();
-    Scanner scanner = new Scanner(System.in);
-        while(str.equals("Flat","HotelSuite,"House")) {
-                System.out.print("Thank you for your choice!");
+    public static void main(String[] args) {
+        accomodations.add(new Flat(90, 3, 5000,
+                2));
+        accomodations.add(new HotelSuite(40, 1, 4500,
+                45));
+        accomodations.add(new House(200, 5, 1000,
+                2, 8000));
+
+        System.out.print("Please choose the type of accomodation");
+        String str = new String();
+        Scanner scanner = new Scanner(System.in);
+        while (str.equals("Flat") || str.equals("HotelSuite") || str.equals("House")) {
+            System.out.print("Thank you for your choice!");
+        }
     }
 }
